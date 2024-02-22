584ac6d4fae604d8ee8c901ceee9e802d1ba276e
junesnow86 <junliang@mail.ustc.edu.cn>
2024-02-22 12:42:39 +0000

    add gemma running script

---
+++ play_gemma.py
@@ -0,0 +1,14 @@
+from transformers import AutoTokenizer, pipeline
+import torch
+
+
+model = "google/gemma-7b-it"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = pipeline('text-generation', model=model, model_kwargs={"torch_dtype": torch.bfloat16}, device="cuda")
+messages = [
+    {"role": "user", "content": "Tell me some knowledge about the TV series SILK.."},
+]
+prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+outputs = pipeline(prompt, max_new_tokens=256, add_special_tokens=True, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+print(outputs[0]["generated_text"][len(prompt):])
